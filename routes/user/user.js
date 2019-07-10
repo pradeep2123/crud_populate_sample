7834a17972b27f0a5693622b6f621e8cdcca47f0
pradeep2123 <you@example.com>
2019-07-10 08:18:56 +0530

    jwt added

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,7 +1,12 @@
 const bcrypt = require('bcrypt-nodejs');
-var User = require('../../models/index').User
+const jwt = require('jsonwebtoken');  
+var User = require('../../models/index')
 
-const createUser = (req,res,next)=>{
+var JWT_SECRET = 'Gli7dFsW-J_82cO-Ed_s_ODeDpAFASPD-ge3qLuI6qT6krM3KjOtTsPysR2PkiP9yUdiJrTLwpeNtEaDhwyQmybGTsUTZ1bxZnZ5bWF9_nW7Tfex6lJxQi1Vwq68RTfzie6xa_N7muxISpLCYd8g_c_zOJcmyjkCdZAW5z0LFBZB9icGmJuOMv-VldgroKxJeIh88jEBWWR3eGGU9ZzprnzH6Wi_GONq2q0DELDzDAjmJDelfK1hBOY2vaSfa0lIlZEhLe2YsFwBAMtuqqBnhT3rxGBWkxq2QhN6Wp2bvuhaYC8-_eoKBBeEW31qz2Z6VDbrtuFZXOXZ9iBs9NCAUQ',
+
+
+
+const createUser = function(req,res,next){
     var user = req.body.user;
     var salt = bcrypt.genSaltSync(8);
     var hash = bcrypt.hashSync(user.password, salt);
@@ -48,7 +53,46 @@ const createUser = (req,res,next)=>{
     })
 }
 
-const getAllUser = (req,res,next)=>{
+const userSignin = (req,res)=>{
+    var user = req.body.user;
+
+    if(req.cookies.token){
+        return res.redirect('/home');
+    }
+    User.findOne({email: user.email}, function(error,old_user){
+        if(error){
+            return res.send({
+                type:"Error",
+                message:"No user has found"
+            })
+        }
+        if(!old_user){
+            return res.send({
+                type:"error",
+                message:"Incorrect Password or mail"
+            })
+        }
+        else if(old_user){
+            if(bcrypt.compareSync(user.password,old_user.password))
+            {
+                var token = jwt.sign({user_id:user._id},JWT_SECRET, {expiresIn: (24*60*60) });
+                res.cookie('token',token);
+                return res.send({
+                    type:"Success",
+                    message:"User successfully logged in"
+                })
+            }
+            else
+            return res.send({
+                type:"Error",
+                message:"No user found or incorrect email"
+            })
+ 
+        }
+    })
+}
+
+const getAllUser = (req,res)=>{
     // var count = req.params.count || req.query.count|| 10
     // var page = req.params.page|| req.query.page|| 1;
     
@@ -92,7 +136,7 @@ const getAllUser = (req,res,next)=>{
     })
 }
 
-const updateUser = (req,res,next)=>{
+const updateUser = (req,res)=>{
     var user = req.body.user;
     if(!user.user_id){
         return res.send({
@@ -101,7 +145,7 @@ const updateUser = (req,res,next)=>{
         })
     }
     User.findById(user.user_id)
-    .exec((error,user_found)=>{
+    .exec(function(error,user_found){
         if(error){
             return res.send({
                 type:"Error",
@@ -140,7 +184,7 @@ const updateUser = (req,res,next)=>{
 
 }
 
-const deleteUser = (req,res,next)=>{
+const deleteUser = (req,res)=>{
     var user = req.body.user
     if(!user.user_id){
         return res.send({
@@ -151,7 +195,7 @@ const deleteUser = (req,res,next)=>{
     User.findOneAndDelete({
         _id:user.user_id
     })
-    .exec((error,user)=>{
+    .exec(function(error,user){
         console.log(user)
         if(error){
             return res.send({
@@ -174,9 +218,50 @@ const deleteUser = (req,res,next)=>{
     })
 }
 
+var userChecker = (req, res, next) => {
+    if (req.cookies.token) {
+        jwt.verify(req.cookies.token, JWT_SECRET, function(error, decoded){
+            if(error){
+                res.clearCookie('token')
+                return res.send({
+                    type:"Error",
+                    code:401,
+                    message:"Please sigin in your account and no jwt "
+                })
+            }
+            else
+            req.user_id = decoded.user_id;
+            User.findById(req.user_id, function(err, user){
+                req.user = user;
+                return next();
+            })
+        })  
+    }  
+    else{
+        return res.send({
+            type:"error",
+            message:"Please signin it"
+        })
+    }
+}
+
+const userSignout = (req,res)=>{
+    if (req.cookies.token) {
+        res.clearCookie('token');
+        return res.send({
+            type:"success",
+            message:"User successfully signed out"
+        })
+    } 
+}
+
 module.exports = {
     createUser:createUser,
     getAllUser:getAllUser,
     updateUser:updateUser,
-    deleteUser:deleteUser
+    deleteUser:deleteUser,
+    userChecker:userChecker,
+    userSignin:userSignin,
+    userSignout:userSignout
 }
+
--- url.js
+++ url.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const User = require('./routes/user/user');
+const User = require('./routes/user/user')
 const Company = require('./routes/company/company')
 const app = express.Router();
 
 
-app.post("/user/create",User.createUser);
-app.get("/user/all",User.getAllUser);
-app.post("/user/update",User.updateUser);
-app.post("/user/delete",User.deleteUser);
+app.post("/user/create",User.userChecker,User.createUser);
+app.get("/user/all",User.userChecker,User.getAllUser);
+app.post("/user/update",User.userChecker,User.updateUser);
+app.post("/user/delete",User.userChecker,User.deleteUser);
+app.post("/user/signin",User.userSignin);
+app.post("/user/signout",User.userChecker,User.userSignout);
+
 
 //Company Creation
 //purpose of creating -> refernce the tables
-app.post("/company/create",Company.createCompany);
-app.get("/company/all",Company.getCompany)
+app.post("/company/create",User.userChecker,Company.createCompany);
+app.get("/company/all",User.userChecker,Company.getCompany)
 
 module.exports = app;
